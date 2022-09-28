0021cb8251669f03b05a7845402cedf9f14bc8be
victoriavrobles <victoriavrobles@gmail.com>
2022-09-28 19:41:49 -0300

    firebase 2

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,16 +1,43 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import { CartContext } from "../../context/CartContext";
 import {Link} from "react-router-dom";
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 import { Button } from '@mui/material';
+import { db } from "../../utils/firebase"
+import {collection, addDoc} from "firebase/firestore";
 import "./style.css"
 
 export const CartContainer = () => {
     const {listaProductosCart, removeItem, clear, precioTotal} = useContext(CartContext);
+    const [idOrden, setIdOrden] = useState();
+
+    const enviarOrden = (event) => {
+        event.preventDefault();
+        const orden = {
+            comprador: {name: event.target[0].value,
+                        phone: event.target[1].value, 
+                        email: event.target[2].value}, 
+                        items: listaProductosCart, 
+                        total: precioTotal()
+        }
+        console.log("orden", orden)
+
+        const queryRef = collection(db, "orders");
+        addDoc(queryRef, orden).then(response=>{console.log("response", response);
+        setIdOrden(response.id)
+        clear();
+        });
+    }
 
     return (
     <div>
+        {idOrden ? 
+        <>
+        <Alert severity="success" sx={{padding: "10px", margin: "10px"}}>Su compra fue realizada! Número de orden: {idOrden}</Alert>
+        </>
+        :
+        <div>
         {
         listaProductosCart.length > 0 ? 
         <div className="cartcont">
@@ -24,7 +51,16 @@ export const CartContainer = () => {
                 ))}
                 <p>Precio total: {precioTotal()}</p>
                 <Button variant="outlined" color="error" sx={{padding: "10px", margin: "10px"}} onClick={()=>clear()}>Vaciar carrito</Button>
-                <Button variant="contained" color="success" sx={{padding: "10px", margin: "10px"}}>Terminar compra</Button>
+
+                <form onSubmit={enviarOrden}>
+                    <label>Nombre: </label>
+                    <input type="text"></input>
+                    <label>Telefono: </label>
+                    <input type="number"></input>
+                    <label>Email: </label>
+                    <input type="email"></input>
+                    <Button type="submit">Enviar orden</Button>
+                </form>
                 </div>
                 : <>
                 <Alert severity="info" sx={{padding: "10px", margin: "10px"}}>El carrito esta vacio</Alert>
@@ -32,5 +68,7 @@ export const CartContainer = () => {
                 </>
         }
     </div>
+}
+</div>
     )
 }
